d51b8a4f434f40c5782a4ae0065fdcc9a9ebd25f
milagrosN13 <giuliana-bacchini@hotmail.com>
2022-05-17 22:46:12 -0300

    17/5/2022
    agregando la función calcular los ángulos de un triangulo

--- log_in.php
+++ log_in.php
@@ -11,9 +11,10 @@
   <!----> 
   <body>
     <nav class="nav nav-tabs">
-      <a class="nav-link" href="index.php">Inicio</a>
-      <a class="nav-link" href="sign_in.php">registrarse</a>
-      <a class="nav-link" href="log_in.php">iniciar sesion</a>
+      <a class="nav-link" href="/index.php">Inicio</a>
+      <a class="nav-link" href="/sign_in.php">registrarse</a>
+      <a class="nav-link" href="/log_in.php">iniciar sesion</a>
+      <a class="nav-link" href="/triangulo/triangulo2.html">triangulo</a>
     </nav>
 
 
--- sign_in.php
+++ sign_in.php
@@ -11,41 +11,41 @@
   <!----> 
   <body>
     <nav class="nav nav-tabs">
-      <a class="nav-link" href="index.php">Inicio</a>
-      <a class="nav-link" href="sign_in.php">registrarse</a>
-      <a class="nav-link" href="log_in.php">iniciar sesion</a>
+      <a class="nav-link" href="/index.php">Inicio</a>
+      <a class="nav-link" href="/sign_in.php">registrarse</a>
+      <a class="nav-link" href="/log_in.php">iniciar sesion</a>
+      <a class="nav-link" href="/triangulo/triangulo2.html">triangulo</a>
     </nav>
-    <article class="formRegistrar">
-      <h1>registrate</h1>
+    <h1>registrate</h1>
         <!--recopilacion de datos del cliente--> 
-      <form name="registro" action="validaciones.php">
-        <div id="nombre">
-          <label for="nombre">nombre</label>
-          <input type="text" name="nombre" required>
-        </div>
-        <div id="apellido">
-          <label for="apellido">apellido</label>
-          <input type="text" name="apellido" required>
-        </div>
-        <div id="contraseña">
-          <label for="contraseña">contraseña</label>
-          <input type="password" name="contraseña" required>
-        </div>
-        <div id="doc">
-          <p>tipo de documento</p>
-          <input type="radio" id="Edni" name="tipoDoc" value="DNI">
-          <label for="tipoDoc">DNI</label>
-          <input type="radio" id="Epasaporte" name="tipoDoc" value="Pasaporte">
-          <label for="tipoDoc">Pasaporte</label>
-        </div>
-        <div id="dni" class="esconder">
-          <input type="number" name="dni">
-        </div>
-        <div id="pasaporte">
-          <input type="number" class="esconder" name="pasaporte">
-        </div>
-        <!--mostrar opcion segun tipoDoc-->
-        <script>
+    <form name="registro" action="validaciones.php">
+      <div class="label">
+        <label for="nombre">nombre</label>
+        <input type="text" name="nombre" required>
+      </div>
+      <div class="label" id="apellido">
+        <label for="apellido">apellido</label>
+        <input type="text" name="apellido" required>
+      </div>
+      <div class="label" id="contraseña">
+        <label for="contraseña">contraseña</label>
+        <input type="password" name="contraseña" required>
+      </div>
+      <div class="label" id="doc">
+        <p>tipo de documento</p>
+        <input type="radio" id="Edni" name="tipoDoc" value="DNI">
+        <label for="tipoDoc">DNI</label>
+        <input type="radio" id="Epasaporte" name="tipoDoc" value="Pasaporte">
+        <label for="tipoDoc">Pasaporte</label>
+      </div>
+      <div class="label" id="dni" >
+        <input type="number" class="esconder" name="dni">
+      </div>
+      <div class="label" id="pasaporte">
+        <input type="number" class="esconder" name="pasaporte">
+      </div>
+      <!--mostrar opcion segun tipoDoc-->
+      <script>
           function mostrar(id){
             id.classList.remove("esconder");
             id.classList.add("mostrar");
@@ -58,55 +58,54 @@
               mostrar(document.getElementById("pasaporte"))
             }
 
-        </script>
-        <div id="mail">
-          <label for="mail">correo</label>
-          <input type="mail" name="mail">
-        </div>
-        <div id="provincia">
-          <label for="provincia">provincia</label>
-          <select name="provincia">
-            <option name="buenosAires" value="buenosAires">Buenos Aires</option>
-            <option name="CbuenosAires" value="CbuenosAires">Ciudad Autónoma de Buenos Aires</option>
-            <option name="catamarca" value="catamarca">Catamarca</option>
-            <option name="chaco" value="chaco">Chaco</option>
-            <option name="chubut" value="chubut">Chubut</option>
-            <option name="cordoba" value="cordoba">Córdoba</option>
-            <option name="corrientes" value="corrientes">Corrientes</option>
-            <option name="entreRios" value="entreRios">Entre Ríos</option>
-            <option name="formosa" value="formosa">Formosa</option>
-            <option name="jujuy" value="jujuy">Jujuy</option>
-            <option name="pampa" value="pampa">La Pampa</option>
-            <option name="rioja" value="rioja">La Rioja</option>
-            <option name="mendoza" value="mendoza">Mendoza</option>
-            <option name="misiones" value="misiones">Misiones</option>
-            <option name="neuquen" value="neuquen">Neuquén</option>
-            <option name="rioNegro" value="rioNegro">Río Negro</option>
-            <option name="salta" value="salta">Salta</option>
-            <option name="sanJuan" value="sanJuan">San Juan</option>
-            <option name="sanLuis"> value="sanLuis">San Luis</option>
-            <option name="santaCluz" value="santaCluz">Santa Cruz</option>
-            <option name="santaFe" value="santaFe">Santa Fe</option>
-            <option name="Santiago" value="Santiago">Santiago del Estero</option>
-            <option name="Tierra del Fuego" value="Tierra del Fuego">Tierra del Fuego, Antártida e Islas del Atlántico Sur</option>
-            <option name="Tucumán" value="Tucumán">Tucumán</option>
-          </select>
-        </div>
-        <div id="estudiante">
-          <p>¿eres estidiante?</p>
-          <input type="checkbox" name="estudiante" value="si">
-          <label for="estudiante">Si</label><br>
-          <input type="checkbox" name="estudiante" value="no">
-          <label for="estudiante">No</label>
-        </div>
-        <div id="comentarios">
-          <p>¿Para que quiere registrarse?</p>
-          <textarea name="comentarios"></textarea>
-        </div>
-        <div id="enviar">
-          <input type="submit" name="enviar" value="enviar">
-        </div>
-      </form>
-    </article>
+      </script>
+      <div class="label" id="mail">
+        <label for="mail">correo</label>
+        <input type="mail" name="mail">
+      </div>
+      <div class="label" id="provincia">
+        <label for="provincia">provincia</label>
+        <select name="provincia">
+          <option name="buenosAires" value="buenosAires">Buenos Aires</option>
+          <option name="CbuenosAires" value="CbuenosAires">Ciudad Autónoma de Buenos Aires</option>
+          <option name="catamarca" value="catamarca">Catamarca</option>
+          <option name="chaco" value="chaco">Chaco</option>
+          <option name="chubut" value="chubut">Chubut</option>
+          <option name="cordoba" value="cordoba">Córdoba</option>
+          <option name="corrientes" value="corrientes">Corrientes</option>
+          <option name="entreRios" value="entreRios">Entre Ríos</option>
+          <option name="formosa" value="formosa">Formosa</option>
+          <option name="jujuy" value="jujuy">Jujuy</option>
+          <option name="pampa" value="pampa">La Pampa</option>
+          <option name="rioja" value="rioja">La Rioja</option>
+          <option name="mendoza" value="mendoza">Mendoza</option>
+          <option name="misiones" value="misiones">Misiones</option>
+          <option name="neuquen" value="neuquen">Neuquén</option>
+          <option name="rioNegro" value="rioNegro">Río Negro</option>
+          <option name="salta" value="salta">Salta</option>
+          <option name="sanJuan" value="sanJuan">San Juan</option>
+          <option name="sanLuis"> value="sanLuis">San Luis</option>
+          <option name="santaCluz" value="santaCluz">Santa Cruz</option>
+          <option name="santaFe" value="santaFe">Santa Fe</option>
+          <option name="Santiago" value="Santiago">Santiago del Estero</option>
+          <option name="Tierra del Fuego" value="Tierra del Fuego">Tierra del Fuego, Antártida e Islas del Atlántico Sur</option>
+          <option name="Tucumán" value="Tucumán">Tucumán</option>
+        </select>
+      </div>
+      <div class="label" id="estudiante">
+        <p>¿eres estidiante?</p>
+        <input type="checkbox" name="estudiante" value="si">
+        <label for="estudiante">Si</label><br>
+        <input type="checkbox" name="estudiante" value="no">
+        <label for="estudiante">No</label>
+      </div>
+      <div class="label" id="comentarios">
+        <p>¿Para que quiere registrarse?</p>
+        <textarea name="comentarios"></textarea>
+      </div>
+      <div id="enviar">
+        <input type="submit" name="enviar" value="enviar">
+      </div>
+    </form>
   </body>
 </html>
