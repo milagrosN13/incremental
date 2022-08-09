5ab419608d43aecad6a5d9fc4468f6f06c3969a6
Giuliana Bacchini <giuliana-bacchini@hotmail.com>
2022-08-09 20:06:29 -0300

    formulario de registro cambiado.

--- formRegistro.php
+++ formRegistro.php
@@ -10,79 +10,26 @@
     <form name="registro" action="">
       <h1>registrate</h1>
       <div class="label" id="nombre">
-        <label for="nombre">nombre</label>
+        nombre
         <input type="text" name="nombre" required>
       </div>
       <div class="label" id="apellido">
-        <label for="apellido">apellido</label>
+        apellido
         <input type="text" name="apellido" required>
       </div>
       <div class="label" id="nombre-usuario">
-        <label for="nombre-usuario">nombre de usuario</label>
+        nombre de usuario
         <input type="text" name="nombre-usuario" required>
       </div>
       <div class="label" id="contraseña">
-        <label for="contraseña">contraseña</label>
+        contraseña
         <input type="password" name="contraseña" required>
       </div>
-      <div class="label" id="doc">
-        <p>tipo de documento</p>
-        <input type="radio" id="Edni" name="tipoDoc" value="DNI">
-        <label for="tipoDoc">DNI</label>
-        <input type="radio" id="Epasaporte" name="tipoDoc" value="Pasaporte">
-        <label for="tipoDoc">Pasaporte</label>
-      </div>
-      <div class="label" id="dni" >
-        <input type="number" class="esconder" name="dni">
-      </div>
-      <div class="label" id="pasaporte">
-        <input type="number" class="esconder" name="pasaporte">
-      </div>
       <div class="label" id="mail">
-        <label for="mail">correo</label>
+        correo
         <input type="mail" name="mail">
       </div>
-      <div class="label" id="provincia">
-        <label for="provincia">provincia</label>
-        <select name="provincia">
-          <option name="buenosAires" value="buenosAires">Buenos Aires</option>
-          <option name="CbuenosAires" value="CbuenosAires">Ciudad Autónoma de Buenos Aires</option>
-          <option name="catamarca" value="catamarca">Catamarca</option>
-          <option name="chaco" value="chaco">Chaco</option>
-          <option name="chubut" value="chubut">Chubut</option>
-          <option name="cordoba" value="cordoba">Córdoba</option>
-          <option name="corrientes" value="corrientes">Corrientes</option>
-          <option name="entreRios" value="entreRios">Entre Ríos</option>
-          <option name="formosa" value="formosa">Formosa</option>
-          <option name="jujuy" value="jujuy">Jujuy</option>
-          <option name="pampa" value="pampa">La Pampa</option>
-          <option name="rioja" value="rioja">La Rioja</option>
-          <option name="mendoza" value="mendoza">Mendoza</option>
-          <option name="misiones" value="misiones">Misiones</option>
-          <option name="neuquen" value="neuquen">Neuquén</option>
-          <option name="rioNegro" value="rioNegro">Río Negro</option>
-          <option name="salta" value="salta">Salta</option>
-          <option name="sanJuan" value="sanJuan">San Juan</option>
-          <option name="sanLuis"> value="sanLuis">San Luis</option>
-          <option name="santaCluz" value="santaCluz">Santa Cruz</option>
-          <option name="santaFe" value="santaFe">Santa Fe</option>
-          <option name="Santiago" value="Santiago">Santiago del Estero</option>
-          <option name="Tierra del Fuego" value="Tierra del Fuego">Tierra del Fuego, Antártida e Islas del Atlántico Sur</option>
-          <option name="Tucumán" value="Tucumán">Tucumán</option>
-        </select>
-      </div>
-      <div class="label" id="estudiante">
-        <p>¿eres estidiante?</p>
-        <input type="checkbox" name="estudiante" value="si">
-        <label for="estudiante">Si</label><br>
-        <input type="checkbox" name="estudiante" value="no">
-        <label for="estudiante">No</label>
-      </div>
-      <div class="label" id="comentarios">
-        <p>¿Para que quiere registrarse?</p>
-        <textarea name="comentarios"></textarea>
-      </div>
-      <div id="enviar">
+      <div class="label" id="enviar">
         <input type="submit" name="enviar" value="enviar">
       </div>
     </form>
--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 /*index*/
-// mostrar opcion segun tipoDoc
-
 
 /*log_in*/
+
 /*sign_in*/
+// mostrar opcion segun tipoDoc
+// si la id es dni hacer un text con el name="dni" si no con name="pasaporte"
+function tipoDoc() {
+    var Valor_tipo = document.getElementById('tipoDoc').nodeValue;
+    if (Valor_tipo == "DNI") {
+        document.getElementById('DNI').hidden = false;
+    }
+    if (Valor_tipo == "pasaporte") {
+        document.getElementById('pasaporte').hidden = false;
+    }
+}
