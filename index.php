4a99841d91f17a1f4e9e804210fe90947568cee8
Giuliana Bacchini <giuliana-bacchini@hotmail.com>
2022-08-09 12:28:05 -0300

    reorganizacion archivos. creacion de plantillas.

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <meta charset="utf-8">
+    <?php require_once "plantillas/head.php" ; ?>
     <title>inicio</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="/script.js"></script>
   </head>
   <body>
-    <?php include_once "./assets/menus.php" ; ?>
+    <?php require_once "plantillas/menus.php" ; ?>
+    
   </body>
 </html>
