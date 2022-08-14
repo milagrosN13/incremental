88b270f175b2109f85b3cb34960a5ea2e373e594
Giuliana Bacchini <giuliana-bacchini@hotmail.com>
2022-08-13 22:04:36 -0300

    pruebas con el menu.

--- plantillas/menus.php
+++ plantillas/menus.php
@@ -1,6 +1,28 @@
+<?php require_once "head.php" ; ?>
 <!--menu para todas las pag-->   <!----> 
 <nav class="nav nav-tabs">
     <a class="nav-link" href="\incremental\index.php">Inicio</a>
     <a class="nav-link" href="\incremental\formInicioSesion.php">iniciar sesion</a>
     <button><a href="\incremental\formRegistro.php">registrarse</a></button>
 </nav>
+<div class="menu-icon-wrapper">
+	<div class="menu-icon" onclick="toggleCssMenu(this)">
+		<div class="three-line">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+</div>
+<div id="cssmenu">
+    <ul>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Creative</a></li>
+        <li><a href="#">Studio</a></li>
+        <li><a href="#">Press</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
+<script>
+
+</script>
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function confirmarDatos(){
-    form = document.getElementsByName("nombre");
-    if (form.value == null){
-        return "esta vacio";
+function toggleCssMenu(icon) {
+    var cssmenu = document.getElementById('cssmenu');
+    if (icon.className.indexOf('active') == -1) {
+        icon.className = 'menu-icon active';
+        cssmenu.style.display = "block";
+        setTimeout(function(){cssmenu.className = 'active';},0);            
+    }
+    else {
+        icon.className = 'menu-icon';
+        cssmenu.className = '';
+        setTimeout(function(){cssmenu.style.display = "none";},411); 
     }
 }
-form = document.getElementsByName("nombre").value;
-regresa = form.addEventListener("change", confirmarDatos);
-alert(regresa)
